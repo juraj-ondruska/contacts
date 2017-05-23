29bdc6c133cb162561c49766207bb6a58adca3c5
Juraj <juro157@gmail.com>
2017-05-23 16:22:34 +0200

    Phase 2

--- main/java/nxp/cz/mycontacts/ContactsActivity.java
+++ main/java/nxp/cz/mycontacts/ContactsActivity.java
@@ -2,29 +2,46 @@ package nxp.cz.mycontacts;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class ContactsActivity extends ListActivity {
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ArrayAdapter<String> valuesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, loadNumbers());
+        setContentView(R.layout.activity_contacts);
+        final ArrayAdapter<String> valuesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, loadNumbers());
         setListAdapter(valuesAdapter);
+        Button addButton = (Button) findViewById(R.id.button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesAdapter.add(createRandomNumber());
+            }
+        });
     }
 
-    private String[] loadNumbers() {
-        Random random = new Random();
-        String[] result = new String[50];
-        for (int i = 0; i < result.length; i++) {
-            result[i] =
-                    Long.toString(700 + random.nextInt(99)) + "-" +
-                    Long.toString(100 + random.nextInt(899)) + "-" +
-                    Long.toString(100 + random.nextInt(899));
+    private List<String> loadNumbers() {
+        int size = 5;
+        ArrayList<String> result = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            result.add(createRandomNumber());
         }
         return result;
+    }
+
+    private String createRandomNumber() {
+        return
+                Long.toString(700 + random.nextInt(99)) + "-" +
+                Long.toString(100 + random.nextInt(899)) + "-" +
+                Long.toString(100 + random.nextInt(899));
     }
 
 }
